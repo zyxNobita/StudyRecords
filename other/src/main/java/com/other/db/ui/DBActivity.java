f021ff6ad6878e0000cb74c6e790447c946f86f5
zhangqiang <18353623706@163.com>
2016-06-17 18:27:22 +0800

    add enum README.md

--- other/src/main/java/com/other/db/ui/DBActivity.java
+++ other/src/main/java/com/other/db/ui/DBActivity.java
@@ -2,15 +2,33 @@ package com.other.db.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.other.R;
 
-public class DBActivity extends AppCompatActivity implements ViewInterface{
+public class DBActivity extends AppCompatActivity implements ViewInterface {
+
+    @butterknife.Bind(R.id.inputTxt)
+    TextView inputTxt;
+    @butterknife.Bind(R.id.btnInit)
+    Button btnInit;
+    @butterknife.Bind(R.id.btnAdd)
+    Button btnAdd;
+    @butterknife.Bind(R.id.btnQuery)
+    Button btnQuery;
+    @butterknife.Bind(R.id.btnDelete)
+    Button btnDelete;
+    @butterknife.Bind(R.id.btnUpdate)
+    Button btnUpdate;
+    @butterknife.Bind(R.id.showTxt)
+    TextView showTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db);
+        butterknife.ButterKnife.bind(this);
     }
 
     @Override
